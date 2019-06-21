8b8f724481cb620047ed9828bf691dea0cb6e557
nyashiki <tnakayashiki01@gmail.com>
2019-06-21 13:59:18 +0900

    Move構造体にto変数を追加

--- minishogilib/src/move.rs
+++ minishogilib/src/move.rs
@@ -6,31 +6,34 @@ use types::*;
 #[derive(Copy, Clone)]
 pub struct Move {
     pub piece: Piece,
-    pub target: u8,            // 移動元 (持ち駒を打つ場合には、打つ場所)
+    pub from: u8,              // 移動元 (持ち駒を打つ場合には、打つ場所)
     pub direction: Direction,  // 移動方向
     pub amount: u8,            // 移動量 (0の場合には持ち駒)
+    pub to: u8,                // 移動先
     pub promotion: bool,       // 成/不成
     pub capture_piece: Piece   // 取った相手の駒
 }
 
 impl Move {
-    pub fn board_move(piece: Piece, target: u8, direction: Direction, amount: u8, promotion: bool, capture_piece: Piece) -> Move {
+    pub fn board_move(piece: Piece, from: u8, direction: Direction, amount: u8, to: u8, promotion: bool, capture_piece: Piece) -> Move {
         Move {
             piece: piece,
-            target: target,
+            from: from,
             direction: direction,
             amount: amount,
+            to: to,
             promotion: promotion,
             capture_piece: capture_piece
         }
     }
 
-    pub fn hand_move(piece: Piece, target: u8) -> Move {
+    pub fn hand_move(piece: Piece, to: u8) -> Move {
         Move {
             piece: piece,
-            target: target,
+            from: 0,
             direction: Direction::N,  // 不使用なので仮の値を入れておく
             amount: 0,
+            to: to,
             promotion: false,
             capture_piece: Piece::NoPiece
         }
@@ -39,9 +42,10 @@ impl Move {
 
 pub static NULL_MOVE: Move = Move {
     piece: Piece::NoPiece,
-    target: SQUARE_NB as u8,
+    from: SQUARE_NB as u8,
     direction: Direction::N,
     amount: 0,
+    to: 0,
     promotion: false,
     capture_piece: Piece::NoPiece
 };
