40cac8e780fd5a986bba5fe740ead3fd39c95b03
nyashiki <tnakayashiki01@gmail.com>
2019-06-21 13:14:47 +0900

    Move構造体のメンバをpublicに変更

--- minishogilib/src/move.rs
+++ minishogilib/src/move.rs
@@ -5,11 +5,11 @@ use types::*;
 #[pyclass]
 #[derive(Copy, Clone)]
 pub struct Move {
-    piece: Piece,
-    target: u8,            // 移動元 (持ち駒を打つ場合には、打つ場所)
-    direction: Direction,  // 移動方向
-    amount: u8,            // 移動量 (0の場合には持ち駒)
-    promotion: bool        // 成/不成
+    pub piece: Piece,
+    pub target: u8,            // 移動元 (持ち駒を打つ場合には、打つ場所)
+    pub direction: Direction,  // 移動方向
+    pub amount: u8,            // 移動量 (0の場合には持ち駒)
+    pub promotion: bool        // 成/不成
 }
 
 impl Move {
