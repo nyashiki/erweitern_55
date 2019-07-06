e993d456eb5c2015056334fc1a48a10c64e7d4a1
nakayashiki <tnakayashiki01@gmail.com>
2019-07-06 16:47:37 +0900

    ADJACENT_ATTACKのバグ修正

--- minishogilib/src/bitboard.rs
+++ minishogilib/src/bitboard.rs
@@ -3,7 +3,7 @@ use position::*;
 
 pub type Bitboard = u32;
 
-static mut ADJACENT_ATTACK: [Bitboard; Piece::BPawnX as usize + 1] = [0; Piece::BPawnX as usize + 1];
+pub static mut ADJACENT_ATTACK: [[Bitboard; Piece::BPawnX as usize + 1]; SQUARE_NB] = [[0; Piece::BPawnX as usize + 1]; SQUARE_NB];
 
 pub fn init() {
     let mut position: Position = Position::empty_board();
@@ -11,6 +11,8 @@ pub fn init() {
     for i in 0..SQUARE_NB {
         for piece in PIECE_ALL.iter() {
             position.board[i] = *piece;
+            position.side_to_move = piece.get_color();
+
             let moves = position.generate_moves(true, false);
 
             for m in moves {
@@ -19,7 +21,7 @@ pub fn init() {
                 }
 
                 unsafe {
-                    ADJACENT_ATTACK[*piece as usize] |= 1 << m.to;
+                    ADJACENT_ATTACK[i][*piece as usize] |= 1 << m.to;
                 }
             }
         }
