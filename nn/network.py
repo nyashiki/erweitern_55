cdca5dfa839b86c612afea03025e05869c3af7ff
nakayashiki <tnakayashiki01@gmail.com>
2019-07-14 23:27:55 +0900

    stepごとに学習

--- nn/network.py
+++ nn/network.py
@@ -12,15 +12,14 @@ class Network:
         x = keras.layers.Conv2D(128, [3, 3], padding='same', activation=tf.nn.relu)(input_image)
 
         # Residual blocks
-        for i in range(3):
+        for i in range(5):
             x = self.residual_block(x)
 
         # Policy head
-        """
         policy = keras.layers.Conv2D(128, [3, 3], padding='same', activation=tf.nn.relu)(x)
         policy = keras.layers.Conv2D(69, [3, 3], padding='same', activation=tf.nn.relu)(policy)
+        policy = keras.layers.Flatten()(policy)
         policy = keras.layers.Softmax(name='policy')(policy)
-        """
 
         # Value head
         value = keras.layers.Conv2D(1, [3, 3], padding='same', activation=tf.nn.relu)(x)
@@ -29,12 +28,13 @@ class Network:
         value = keras.layers.Dense(1, activation=tf.nn.tanh, name='value')(value)
 
         # 入力層、出力層を定義
-        self.model = keras.Model(inputs=input_image, outputs=[value])
+        self.model = keras.Model(inputs=input_image, outputs=[policy, value])
 
         # optimizerを定義
         self.model.compile(optimizer=tf.keras.optimizers.SGD(lr=1e-4, decay=1e-6, momentum=0.9),
-                    loss={'value': keras.losses.mean_squared_error},
-                    loss_weights={'value': 1})
+                    loss={'policy': keras.losses.categorical_crossentropy,
+                          'value': keras.losses.mean_squared_error},
+                    loss_weights={'policy': 1, 'value': 1})
 
     def residual_block(self, input_image, conv_kernel_shape=[3, 3]):
         conv_filters = int(input_image.shape[3])
@@ -49,9 +49,9 @@ class Network:
 
         return x
 
-    def step(self, train_images, policy_label, value_label, batch_size, epochs=1):
-        print('debug', train_images.shape)
-        self.model.fit(train_images, [value_label], batch_size=batch_size, epochs=epochs)
+    def step(self, train_images, policy_label, value_label):
+        loss = self.model.train_on_batch(train_images, [policy_label, value_label])
+        return loss
 
     def predict(self, images):
         return self.model.predict(images, batch_size=len(images), verbose=1, steps=None)
