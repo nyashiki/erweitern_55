53df337e1e73dd9e723b5d1d9cc81b5a3e87aae2
nakayashiki <tnakayashiki01@gmail.com>
2019-07-29 14:16:48 +0900

    rust mcts

--- main.py
+++ main.py
@@ -15,10 +15,15 @@ def run_mcts(position, nn):
     mcts = minishogilib.MCTS()
 
     root = mcts.set_root()
-    mcts.evaluate(root, position, np.random.rand(69 * 5 * 5).astype('f'), np.random.rand())
+    nninput = position.to_nninput().reshape((1, 134, 5, 5))
+    nninput = np.transpose(nninput, axes=[0, 2, 3, 1])
+    policy, value = nn.predict(nninput)
+    value = (value + 1) / 2
+
+    mcts.evaluate(root, position, policy[0], value[0][0])
 
     SIMULATION_NUM = 800
-    BATCH_SIZE = 8
+    BATCH_SIZE = 32
 
     values = [None for _ in range(BATCH_SIZE)]
     leaf_nodes = [None for _ in range(BATCH_SIZE)]
@@ -26,16 +31,24 @@ def run_mcts(position, nn):
 
     for _ in range(SIMULATION_NUM // BATCH_SIZE):
         for b in range(BATCH_SIZE):
-            leaf_positions[b] = position.copy(False)
+            leaf_positions[b] = position.copy(True)
             leaf_nodes[b] = mcts.select_leaf(root, leaf_positions[b])
 
-        # neural network here
+        # use neural network to evaluate the position
+        nninputs = np.zeros((BATCH_SIZE, 134, 5, 5))
+        for b in range(BATCH_SIZE):
+            nninputs[b] = leaf_positions[b].to_nninput().reshape((1, 134, 5, 5))
+        nninputs = np.transpose(nninputs, axes=[0, 2, 3, 1])
+        policy, value = nn.predict(nninputs)
+        value = (value + 1) / 2
 
         for b in range(BATCH_SIZE):
-            values[b] = mcts.evaluate(leaf_nodes[b], leaf_positions[b], np.random.rand(69 * 5 * 5).astype('f'), np.random.rand())
+            values[b] = mcts.evaluate(leaf_nodes[b], leaf_positions[b], policy[b], value[b][0])
 
         for b in range(BATCH_SIZE):
             mcts.backpropagate(leaf_nodes[b], values[b])
+
+    mcts.print(root)
 
     return mcts.best_move(root)
 
@@ -74,7 +87,8 @@ def main():
     position.set_start_position()
 
     neural_network = network.Network()
-    neural_network.load('./nn/weights/epoch_052.h5')
+    # neural_network.load('./nn/weights/epoch_052.h5')
+    # neural_network.load('./nn/weights_old/epoch_99.h5')
 
     # 1回predictを行い，1回目の実行が遅くならないようにする
     random_input = np.random.rand(1, 5, 5, 134)
@@ -88,6 +102,7 @@ def main():
         if best_move.is_null_move():
             break
 
+        print(best_move)
         position.do_move(best_move)
 
         print('--------------------')
@@ -104,6 +119,16 @@ def main():
     # for (k, v) in root.children.items():
     #     print(k, '\n  N:', v.N, 'P:', v.P, 'V:', v.V)
 
+def debug():
+    position = minishogilib.Position()
+    position.set_sfen("2k2/5/2P2/5/2K2 b G 1")
+
+    neural_network = network.Network()
+    best_move = run_mcts(position, neural_network)
+    print(best_move)
+    position.do_move(best_move)
+    position.print()
+
 if __name__ == '__main__':
     # output minishogilib version
     print(minishogilib.version())
@@ -111,4 +136,6 @@ if __name__ == '__main__':
     # fix the seed
     np.random.seed(0)
 
-    main()
+    # main()
+
+    debug()
