3e465224c61f75b919ed78e5c7b2510089b5e8d4
nakayashiki <tnakayashiki01@gmail.com>
2019-08-04 16:22:29 +0900

    implement some fundamental methods

--- python/reservoir.py
+++ python/reservoir.py
@@ -1,6 +1,19 @@
+import pickle
+
 class Reservoir:
     def __init__(self):
         self.records = []
+
+    def push(self, record):
+        self.records.append(record)
+
+    def save(self, path):
+        with open(path, 'wb') as f:
+            pickle.dump(self.records, f, protocol=2)
+
+    def load(self, path):
+        with open(path, 'rb') as f:
+            self.records = pickle.load(f)
 
     def sample(self, size):
         pass  # ToDo
