0cd614a29cb3dbabe08131643021643774f15635
nakayashiki <tnakayashiki01@gmail.com>
2019-08-04 19:42:48 +0900

    make Trainer class

--- python/train.py
+++ python/train.py
@@ -1,5 +1,6 @@
 import datetime
 import socket
+from optparse import OptionParser
 import pickle
 import sys
 
@@ -8,43 +9,68 @@ from nn import network
 from reservoir import Reservoir
 import utils
 
-def run():
-    reservoir = Reservoir()
-    nn = network.Network()
-    nn.load('./nn/weights/epoch_030.h5')
+class Trainer:
+    def __init__(self):
+        self.reservoir = Reservoir()
+        self.nn = network.Network()
 
-    sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sc.bind(('localhost', 9000))
-    sc.listen(128)
+    def collect_records(self, port):
+        self.nn.load('./nn/weights/epoch_030.h5')
 
-    print('Ready')
+        sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sc.bind(('localhost', port))
+        sc.listen(128)
 
-    log_file = open('connection_log.txt', 'w')
+        print('Ready')
 
-    while True:
-        conn, addr = sc.accept()
-        message = conn.recv(1024)
+        log_file = open('connection_log.txt', 'w')
 
-        if message == b'parameter':
-            data = pickle.dumps(nn.model.get_weights(), protocol=2)
-            conn.send(sys.getsizeof(data).to_bytes(16, 'little'))
-            conn.send(data)
+        while True:
+            conn, addr = sc.accept()
+            message = conn.recv(1024)
 
-            log_file.write('[{}] sent the parameters to {}\n'.format(datetime.datetime.now(datetime.timezone.utc), str(addr)))
+            if message == b'parameter':
+                data = pickle.dumps(self.nn.model.get_weights(), protocol=2)
+                conn.send(sys.getsizeof(data).to_bytes(16, 'little'))
+                conn.send(data)
 
-        elif message == b'record':
-            data = conn.recv(16)
-            data_size = int.from_bytes(data, 'little')
+                log_file.write('[{}] sent the parameters to {}\n'.format(datetime.datetime.now(datetime.timezone.utc), str(addr)))
 
-            data = utils.recvall(conn, data_size)
+            elif message == b'record':
+                data = conn.recv(16)
+                data_size = int.from_bytes(data, 'little')
 
-            game_record = pickle.loads(data)
-            reservoir.push(game_record)
+                data = utils.recvall(conn, data_size)
 
-            log_file.write('[{}] received a game record from {}\n'.format(datetime.datetime.now(datetime.timezone.utc), str(addr)))
+                game_record = pickle.loads(data)
+                reservoir.push(game_record)
 
-            if reservoir.len() % 10 == 0:
-                reservoir.save('records.pkl')
+                log_file.write('[{}] received a game record from {}\n'.format(datetime.datetime.now(datetime.timezone.utc), str(addr)))
 
-        log_file.flush()
-        conn.close()
+                if self.reservoir.len() % 10 == 0:
+                    self.reservoir.save('records.pkl')
+
+            log_file.flush()
+            conn.close()
+
+    def update_parameters(self):
+        BATCH_SIZE = 1024
+
+        mini_batch = self.reservoir.sample(BATCH_SIZE)
+
+        # Convert sfen_kifs and mcts_outputs to the neural network format
+        # ToDo:
+
+        # Update neural network parameters
+
+
+
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option('-p', '--port', dest='port', help='port')
+
+    trainer = Trainer()
+    trainer.reservoir.load('records.pkl')
+
+    # Make the server which receives game records by selfplay from clients
+    # trainer.collect_records()
