6d55bbf87d3d4f391bcfecdd105a6f4bebd92afd
nakayashiki <tnakayashiki01@gmail.com>
2019-08-04 15:09:29 +0900

    use abstractmethod

--- python/search.py
+++ python/search.py
@@ -1,9 +1,14 @@
+from abc import ABCMeta, abstractmethod
+
 class Search:
+    @abstractmethod
     def run(self):
         raise NotImplementedError
 
+    @abstractmethod
     def best_move(self):
         raise NotImplementedError
 
+    @abstractmethod
     def dump(self):
         raise NotImplementedError
