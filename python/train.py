38a44dcc9ca73f996bb8ccc023e4b23aed4fe71f
nakayashiki <tnakayashiki01@gmail.com>
2019-08-05 01:51:43 +0900

    construct the server and train the neural network in parallel

--- python/train.py
+++ python/train.py
@@ -3,23 +3,36 @@ import socket
 from optparse import OptionParser
 import pickle
 import sys
+import threading
 
 import mcts
 from nn import network
 from reservoir import Reservoir
+import tensorflow as tf
+import tensorflow.keras.backend as K
 import utils
 
-class Trainer:
-    def __init__(self):
+class Trainer():
+    def __init__(self, port):
+        self.port = port
+
         self.reservoir = Reservoir()
         self.nn = network.Network()
+        self.nn.model._make_predict_function()
+        self.session = K.get_session()
+        self.graph = tf.compat.v1.get_default_graph()
+
         self.steps = 0
 
-    def collect_records(self, port):
-        self.nn.load('./nn/weights/epoch_030.h5')
+        self.nn_lock = threading.Lock()
+        self.reservoir_lock = threading.Lock()
 
+        # self.nn.load('./nn/weights/epoch_030.h5')
+        # self.reservoir.load('records.pkl')
+
+    def collect_records(self):
         sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sc.bind(('localhost', port))
+        sc.bind(('localhost', self.port))
         sc.listen(128)
 
         print('Ready')
@@ -31,9 +44,12 @@ class Trainer:
             message = conn.recv(1024)
 
             if message == b'parameter':
-                data = pickle.dumps(self.nn.model.get_weights(), protocol=2)
-                conn.send(sys.getsizeof(data).to_bytes(16, 'little'))
-                conn.send(data)
+                with self.nn_lock:
+                    with self.session.as_default():
+                        with self.graph.as_default():
+                            data = pickle.dumps(self.nn.model.get_weights(), protocol=2)
+                            conn.send(sys.getsizeof(data).to_bytes(16, 'little'))
+                            conn.send(data)
 
                 log_file.write('[{}] sent the parameters to {}\n'.format(datetime.datetime.now(datetime.timezone.utc), str(addr)))
 
@@ -43,13 +59,15 @@ class Trainer:
 
                 data = utils.recvall(conn, data_size)
 
-                game_record = pickle.loads(data)
-                reservoir.push(game_record)
+                with self.reservoir_lock:
+                    game_record = pickle.loads(data)
+                    self.reservoir.push(game_record)
 
                 log_file.write('[{}] received a game record from {}\n'.format(datetime.datetime.now(datetime.timezone.utc), str(addr)))
 
                 if self.reservoir.len() % 10 == 0:
-                    self.reservoir.save('records.pkl')
+                    with reservoir_lock:
+                        self.reservoir.save('records.pkl')
 
             log_file.flush()
             conn.close()
@@ -58,28 +76,36 @@ class Trainer:
         BATCH_SIZE = 1024
         RECENT_GAMES = 50000
 
-        self.nn.load('./nn/weights/epoch_030.h5')
-        self.reservoir.load('records.pkl')
-
         log_file = open('training_log.txt', 'w')
 
         while True:
-            nninputs, policies, values = self.reservoir.sample(BATCH_SIZE, RECENT_GAMES)
+            with self.reservoir_lock:
+                nninputs, policies, values = self.reservoir.sample(BATCH_SIZE, RECENT_GAMES)
 
             # Update neural network parameters
-            loss_sum, policy_loss, value_loss = self.nn.step(nninputs, policies, values)
+            with self.nn_lock:
+                with self.session.as_default():
+                    with self.graph.as_default():
+                        loss_sum, policy_loss, value_loss = self.nn.step(nninputs, policies, values)
 
             log_file.write('{}, {}, {}, {}\n'.format(datetime.datetime.now(datetime.timezone.utc), str(loss_sum), str(policy_loss), str(value_loss)))
             log_file.flush()
+
+    def run(self):
+        # Make the server which receives game records by selfplay from clients
+        collect_records_thread = threading.Thread(target=self.collect_records)
+
+        # Continue to update the neural network parameters
+        update_parameters_thread = threading.Thread(target=self.update_parameters)
+
+        collect_records_thread.start()
+        update_parameters_thread.start()
 
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option('-p', '--port', dest='port', help='port')
 
-    trainer = Trainer()
+    (options, args) = parser.parse_args()
 
-    # Make the server which receives game records by selfplay from clients
-    # trainer.collect_records()
-
-    # Continue to update the neural network parameters
-    trainer.update_parameters()
+    trainer = Trainer(int(options.port))
+    trainer.run()
