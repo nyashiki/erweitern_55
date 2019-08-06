84111dbfc938cc8655dd3024df0e26813785642c
nakayashiki <tnakayashiki01@gmail.com>
2019-08-06 12:57:56 +0900

    view selfplay

--- python/main.py
+++ python/main.py
@@ -12,17 +12,18 @@ import mcts
 from nn import network
 import selfplay
 
+import train
+
 def main():
     neural_network = network.Network()
-    neural_network.load('./nn/weights/epoch_030.h5')
+    neural_network.load('./weights/iter_230000.h5')
 
     # do predict once because the first prediction takes more time than latter one
     random_input = np.random.rand(1, network.INPUT_CHANNEL, 5, 5)
     neural_network.predict(random_input)
 
     search = mcts.MCTS(mcts.Config())
-
-    selfplay.run(neural_network, search, True)
+    selfplay.run(neural_network, search, selfplay.SelfplayConfig(), True)
 
 if __name__ == '__main__':
     print(minishogilib.__version__)
