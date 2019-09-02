14201ae0df6fc2c9a7226e85fe5e0c7442c02758
nakayashiki <tnakayashiki01@gmail.com>
2019-09-02 20:07:00 +0900

    set batch size to be 4096 as default

--- python/train.py
+++ python/train.py
@@ -89,7 +89,7 @@ class Trainer():
             conn.close()
 
     def update_parameters(self):
-        BATCH_SIZE = 2048
+        BATCH_SIZE = 4096
         RECENT_GAMES = 100000
 
         log_file = open('training_log.txt', 'w')
