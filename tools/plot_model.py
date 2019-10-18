4312fce3ff75bef7d41f9b64eafdab8f1ed5cda1
nakayashiki <tnakayashiki01@gmail.com>
2019-10-18 17:26:49 +0900

    basic plot

--- tools/plot_model.py
+++ tools/plot_model.py
@@ -7,7 +7,7 @@ def main():
     nn = network.Network()
     tf.keras.utils.plot_model(
         nn.model, show_shapes=True, show_layer_names=True)
-
+    nn.model.summary()
 
 if __name__ == '__main__':
     main()
