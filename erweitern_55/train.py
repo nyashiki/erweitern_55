38d5895efd0465dc78ce3c6e30a5f3c323a786af
nakayashiki <tnakayashiki01@gmail.com>
2019-11-03 13:37:31 +0900

    change lr

--- erweitern_55/train.py
+++ erweitern_55/train.py
@@ -147,9 +147,9 @@ class Trainer():
             with self.nn_lock:
                 if self.nn.iter() < 100000:
                     learning_rate = 1e-1
-                elif self.nn.iter() < 300000:
+                elif self.nn.iter() < 200000:
                     learning_rate = 1e-2
-                elif self.nn.iter() < 500000:
+                elif self.nn.iter() < 300000:
                     learning_rate = 1e-3
                 else:
                     learning_rate = 1e-4
