67ea22fc8b9c80e04c9044d08c5c2234c9e7aba4
nakayashiki <tnakayashiki01@gmail.com>
2019-12-16 00:08:53 +0900

    change batch size

--- erweitern_55/train.py
+++ erweitern_55/train.py
@@ -51,7 +51,7 @@ class Trainer():
         self.new_record_count_lock = threading.Lock()
 
     def _sample_datasets(self):
-        BATCH_SIZE = 2048
+        BATCH_SIZE = 8192
         flat_sampling = False
 
         while True:
