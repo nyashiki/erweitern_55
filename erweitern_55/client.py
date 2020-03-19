fb6e6e58f4a392e0a692a9583c32b54bc62bf55e
nakayashiki <tnakayashiki01@gmail.com>
2020-03-19 22:25:06 +0900

    wip

--- erweitern_55/client.py
+++ erweitern_55/client.py
@@ -1,64 +1,94 @@
+import minishogilib
+import numpy as np
 from optparse import OptionParser
 import os
 import _pickle
 import sys
 import threading
+import time
 import urllib.request
 
-import mcts
+import gamerecord
 import network
-import selfplay
 
 
 class Client:
     """Client that connects the server and conducts selfplay games.
     """
 
-    def __init__(self, ip, port, update=True, cpu_only=False, update_iter=1, random_play=False, checkmate_depth=7):
+    def __init__(self, ip, port, update=True, cpu_only=False, checkmate_depth=7, worker_num=1):
         self.host = ip
         self.port = port
         self.nn = None
         self.update = update
         self.cpu_only = cpu_only
-        self.update_iter = update_iter
-        self.random_play = random_play
         self.checkmate_depth = checkmate_depth
+        self.worker_num = worker_num
 
     def run(self):
-        mcts_config = mcts.Config()
-        mcts_config.batch_size = 16
-        mcts_config.simulation_num = 800
-        mcts_config.forced_playouts = False
-        mcts_config.use_dirichlet = True
-        mcts_config.reuse_tree = True
-        mcts_config.target_pruning = False
-        mcts_config.immediate = False
-
-        search = mcts.MCTS(mcts_config)
+        memory_size = 0.01
+        simulation_num = 800
 
         device = 'cpu' if self.cpu_only else 'gpu'
         self.nn = network.Network(device)
 
-        iter = 0
+        positions = [minishogilib.Position() for _ in range(self.worker_num)]
+        searchs = [minishogilib.MCTS(memory_size) for _ in range(self.worker_num)]
+        game_records = [gamerecord.GameRecord() for _ in range(self.worker_num)]
+        ins = np.zeros([self.worker_num] + self.nn.input_shape)
+
+        for position in positions:
+            position.set_start_position()
 
         while True:
             # Ask the server the current neural network parameters.
-            if self.update and iter % self.update_iter == 0:
+            if self.update:
                 url = 'http://{}:{}/weight'.format(self.host, self.port)
                 req = urllib.request.Request(url)
                 with urllib.request.urlopen(req) as res:
                     weights = _pickle.loads(res.read())
                     self.nn.model.set_weights(weights)
 
-            # Conduct selfplay.
-            if self.random_play:
-                game_record = selfplay.random_play(
-                    stop_with_checkmate=False)
+            start_time = time.time()
+            # MCTS begin.
+            # Step 1: Set the root nodes.
+            roots = [None for _ in range(self.worker_num)]
+            for (i, position) in enumerate(positions):
+                root = searchs[i].set_root(position, True)
+                roots[i] = root
 
-            else:
-                search.clear()
-                game_record = selfplay.run(
-                    self.nn, search, checkmate_depth=self.checkmate_depth, stop_with_checkmate=False, verbose=True)
+                if not searchs[i].expanded(root):
+                    ins[i] = self.nn.get_input(position)
+
+            # Step 2: Evaluate the root nodes.
+            policy, value = self.nn.predict(ins)
+            value = (value + 1) / 2
+            for (i, position) in enumerate(positions):
+                searchs[i].evaluate(roots[i], position, policy[i], value[i][0])
+
+                # Add dirichlet noise.
+                searchs[i].add_noise(roots[i])
+
+            for _ in range(simulation_num):
+                leaf_nodes = [None for _ in range(self.worker_num)]
+                leaf_positions = [positions[i].copy(True) for i in range(self.worker_num)]
+
+                # Select leaf nodes.
+                for i in range(self.worker_num):
+                    leaf_nodes[i] = searchs[i].select_leaf(roots[i], leaf_positions[i], False)
+                    ins[i] = self.nn.get_input(leaf_positions[i])
+
+                # Evaluate leaf nodes and backpropagate values.
+                policy, value = self.nn.predict(ins)
+                value = (value + 1) / 2
+                for i in range(self.worker_num):
+                    searchs[i].evaluate(leaf_nodes[i], leaf_positions[i], policy[i], value[i][0])
+                    searchs[i].backpropagate(leaf_nodes[i])
+
+            # MCTS end.
+            elapsed = time.time() - start_time
+            print('elapsed:', elapsed)
+            break
 
             # Send result.
             url = 'http://{}:{}/record'.format(self.host, self.port)
@@ -67,7 +97,6 @@ class Client:
             with urllib.request.urlopen(req) as res:
                 pass
 
-            iter += 1
 
 
 if __name__ == '__main__':
@@ -80,15 +109,13 @@ if __name__ == '__main__':
                       help='If true, neural network parameters will not be updated.')
     parser.add_option('-c', '--cpu', action='store_true', dest='cpu_only', default=False,
                       help='If true, use CPU only.')
-    parser.add_option('-u', '--update-iter', dest='update_iter', type='int',
-                      default=1, help='The iteration to update neural network parameters.')
-    parser.add_option('-r', '--random-play', action='store_true', dest='random_play',
-                      default=False, help='Games are conducted by random play.')
     parser.add_option('-d', '--checkmate-depth', dest='checkmate_depth',
                       default=7, help='The depth of checkmate searching.')
+    parser.add_option('-w', '--worker-num', type='int', dest='worker_num',
+                      default=1, help='The number of workers.')
 
     (options, args) = parser.parse_args()
 
     client = Client(options.ip, options.port,
-                    not options.no_update, options.cpu_only, options.update_iter, options.random_play, options.checkmate_depth)
+                    not options.no_update, options.cpu_only, options.checkmate_depth, options.worker_num)
     client.run()
